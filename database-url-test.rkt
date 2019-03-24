2369df2bbe61f1c7deb99e5ff6bf5cfe762cc391
Lassi Kortela <lassi@lassi.io>
2019-03-24 17:40:43 +0200

    Initial commit

---
+++ database-url-test.rkt
@@ -0,0 +1,67 @@
+#lang racket/base
+
+;; Copyright 2019 Lassi Kortela
+;; SPDX-License-Identifier: ISC
+
+(require
+ db
+ rackunit
+ "database-url.rkt")
+
+(define (check-database-url wanted-connect u wanted-kw-alist)
+  (let-values (((kw-hash connect) (database-url-parse u)))
+    (check-equal? connect wanted-connect)
+    (check-equal? (sort (hash->list kw-hash) keyword<? #:key car)
+                  wanted-kw-alist)))
+
+(test-case "mysql"
+  (check-database-url
+   mysql-connect
+   "mysql://user:pass@localhost/dbname"
+   '((#:database . "x")
+     (#:password . "pass")
+     (#:port . #f)
+     (#:server . "localhost")
+     (#:user . "user")))
+  (check-database-url
+   mysql-connect
+   "mysql:/var/run/mysqld/mysqld.sock"
+   '((#:socket . "x/x/x/x"))))
+
+(test-case "postgresql"
+  (check-database-url
+   postgresql-connect
+   "pg://user:pass@localhost/dbname?sslmode=disable"
+   '((#:database . "x")
+     (#:password . "pass")
+     (#:port . #f)
+     (#:server . "localhost")
+     (#:ssl . no)
+     (#:user . "user")))
+  (check-database-url
+   postgresql-connect
+   "postgres://user:pass@localhost/dbname"
+   '((#:database . "x")
+     (#:password . "pass")
+     (#:port . #f)
+     (#:server . "localhost")
+     (#:user . "user")))
+  (check-database-url
+   postgresql-connect
+   "postgresql://user:pass@localhost/mydatabase/?sslmode=disable"
+   '((#:database . "x/x")
+     (#:password . "pass")
+     (#:port . #f)
+     (#:server . "localhost")
+     (#:ssl . no)
+     (#:user . "user"))))
+
+(test-case "sqlite"
+  (check-database-url
+   sqlite3-connect
+   "sqlite:/path/to/file.db"
+   '((#:database . "x/x/x")))
+  (check-database-url
+   sqlite3-connect
+   "sqlite:mydatabase.sqlite3"
+   '((#:database . "x"))))
---
+++ database-url.rkt
@@ -0,0 +1,110 @@
+#lang racket/base
+
+;; Copyright 2019 Lassi Kortela
+;; SPDX-License-Identifier: ISC
+
+(require
+ db
+ net/url
+ racket/match
+ racket/string)
+
+(define (apply-kw-hash proc kw-hash)
+  (let* ((keys (sort (hash-keys kw-hash) keyword<?))
+         (vals (map (lambda (key) (hash-ref kw-hash key))
+                    keys)))
+    (keyword-apply proc keys vals '())))
+
+(define (url-path-as-we-want u)
+  (if (not (url-path u))
+      #f
+      (string-join
+       (map (lambda (u-path-component) "x")
+            (url-path u))
+       "/")))
+
+(define (coerce-to-url u)
+  (cond ((url? u) u)
+        ((string? u) (string->url u))
+        ((not u) (string->url (or (getenv "DATABASE_URL")
+                                  (error "DATABASE_URL not set"))))
+        (else (error "Argument is not a string or URL"))))
+
+(define (parse-user-and-password! u kw)
+  (match (string-split (or (url-user u) "") ":")
+    ((list) #f)
+    ((list user)
+     (hash-set! kw '#:user user))
+    ((list user password)
+     (hash-set! kw '#:user user)
+     (hash-set! kw '#:password password))
+    (else
+     "Too many colons in user:password part of URL")))
+
+(define (parse-server! u kw)
+  (cond ((string? (url-host u))
+         (hash-set! kw '#:server (url-host u))
+         (hash-set! kw '#:port (url-port u))
+         (hash-set! kw '#:database (url-path-as-we-want u)))
+        (else
+         (hash-set! kw '#:socket (url-path-as-we-want u)))))
+
+(define (parse-sslmode! u kw)
+  (match (assoc 'sslmode (url-query u))
+    (#f #f)
+    ((cons sslmode "enable")  (hash-set! kw '#:ssl 'yes))
+    ((cons sslmode "disable") (hash-set! kw '#:ssl 'no))
+    (else  (error "Unknown sslmode argument in database URL"))))
+
+(define (parse-path-only! u kw)
+  (when (url-host u)
+    (error "URL must not have a host"))
+  (when (url-port u)
+    (error "URL must not have a port"))
+  (unless (url-path-as-we-want u)
+    (error "URL must have a path"))
+  (hash-set! kw '#:database (url-path-as-we-want u)))
+
+(define (parser connect . steps)
+  (lambda (u)
+    (let ((kw-hash (make-hash)))
+      (for-each (lambda (step!) (step! u kw-hash))
+                steps)
+      (values kw-hash connect))))
+
+(define mysql-parser
+  (parser mysql-connect
+          parse-user-and-password!
+          parse-server!))
+
+(define postgresql-parser
+  (parser postgresql-connect
+          parse-user-and-password!
+          parse-server!
+          parse-sslmode!))
+
+(define sqlite-parser
+  (parser sqlite3-connect
+          parse-path-only!))
+
+(define schemes
+  (make-hash
+   (list (cons "mysql" mysql-parser)
+         (cons "pg" postgresql-parser)
+         (cons "postgres" postgresql-parser)
+         (cons "postgresql" postgresql-parser)
+         (cons "sqlite" sqlite-parser))))
+
+(define (database-url-parse u)
+  (let* ((u (coerce-to-url u))
+         (f (or (hash-ref schemes (url-scheme u) #f)
+                (error "Unknown database URL scheme"))))
+    (apply f (list u))))
+
+(define (database-url-connector u)
+  (let-values (((kw-hash connect) (database-url-parse u)))
+    (lambda () (apply-kw-hash connect kw-hash))))
+
+(provide
+ database-url-parse
+ database-url-connector)
