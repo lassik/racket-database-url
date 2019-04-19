606122209ec9062019582f1d0dfb08ed8d9d6dde
Lassi Kortela <lassi@lassi.io>
2019-04-19 11:58:03 +0300

    Add build deps for documentation

--- info.rkt
+++ info.rkt
@@ -1,8 +1,15 @@
 #lang info
 
 (define collection "database-url")
-(define deps '("db-lib"
-               "rackunit-lib"
-               "base"))
+
+(define deps
+  '("db-lib"
+    "rackunit-lib"
+    "base"))
+
+(define build-deps
+  '("db-doc"
+    "racket-doc"
+    "scribble-lib"))
 
 (define scribblings '(("database-url.scrbl")))
